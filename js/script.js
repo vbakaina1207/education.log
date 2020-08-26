c9b2b763c0f5dfbf754f4f8511c5515f873fde90
Victoria <v_bakakina@ukr.net>
2020-08-26 15:47:58 +0300

    мобильное меню

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ console.log(mobile__menu_open);
 
 
 document.getElementById("mobile__menu_open_btn").onclick = function () {
-    document.getElementById("mobile__menu_open_btn").classList.add("active");
-    document.getElementById("mobile__menu").classList.add("active");
+    document.getElementById("mobile__menu_open_btn").classList.toggle("active");
+    document.getElementById("mobile__menu").classList.toggle("active");
 }
 
-document.getElementById("mobile__menu_closed_btn").onclick = function (){
+/*document.getElementById("mobile__menu_closed_btn").onclick = function (){
     document.getElementById("mobile__menu_open_btn").classList.remove("active");
     document.getElementById("mobile__menu").classList.remove("active");
-}
+}*/
